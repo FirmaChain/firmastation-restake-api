2da3c747b4f0b7eff5b03b684e6f8f1e34b84b62
thismars <93503020+thismars@users.noreply.github.com>
2022-09-01 15:59:04 +0900

    update: change schedule date

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,15 +1,10 @@
 const ScheduleDate = () => {
   const next = () => {
     let nowDate = new Date();
-    // let hours = nowDate.getUTCHours();
-    // let newHours = Math.floor(hours / 4) * 4 + 4;
-    // nowDate.setUTCHours(newHours);
-    // nowDate.setUTCMinutes(0);
-    // nowDate.setUTCSeconds(0);
-
-    let minutes = nowDate.getUTCMinutes();
-    let newMinutes = Math.floor(minutes / 5) * 5 + 5;
-    nowDate.setUTCMinutes(newMinutes);
+    let hours = nowDate.getUTCHours();
+    let newHours = Math.floor(hours / 4) * 4 + 4;
+    nowDate.setUTCHours(newHours);
+    nowDate.setUTCMinutes(0);
     nowDate.setUTCSeconds(0);
 
     return nowDate.toISOString();
